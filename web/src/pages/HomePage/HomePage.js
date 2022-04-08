1a46b9e9d9402f86960e6bef7a2bc877e9b1ca55
AAWaheed <arbab@aawahede.com>
2022-04-08 15:32:58 +0300

    Scaffolded posts

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+      <i>Home</i>
+    </>
+  )
+}
+
+export default HomePage
